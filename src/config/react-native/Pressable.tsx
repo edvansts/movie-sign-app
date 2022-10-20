9310b1508c43b38e1eb9eaa5ff0ca289f6aa6e26
Edvan de Matos <edvan.stt02@gmail.com>
2022-10-20 02:01:00 -0300

    chore: :wrench: pressable component configured

---
+++ src/config/react-native/Pressable.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import {
+  Pressable as RNPressable,
+  PressableProps as RNPressableProps,
+  StyleProp,
+  ViewStyle,
+} from "react-native";
+
+export interface PressableProps extends RNPressableProps {
+  style?: StyleProp<ViewStyle>; // override to standard ViewStyle
+  pressStyle?: ViewStyle; // optional style object for when pressed
+}
+
+type MergePressableStylesFn = (
+  style?: StyleProp<ViewStyle>,
+  pressStyle?: ViewStyle
+) => RNPressableProps["style"];
+
+const mergePressableStyles: MergePressableStylesFn = (style, pressStyle) => {
+  if (!pressStyle) {
+    return style;
+  }
+
+  if (!style) {
+    return ({ pressed }) => (pressed ? pressStyle : undefined);
+  }
+
+  return ({ pressed }) => (pressed ? [style, pressStyle] : style);
+};
+
+export const Pressable = ({ style, pressStyle, ...props }: PressableProps) => (
+  <RNPressable style={mergePressableStyles(style, pressStyle)} {...props} />
+);
