b479c6593e62953be10663fa673cb9156f3ca8e8
Luccas Ribeiro Cruz <luccas_185@hotmail.com>
2023-01-23 12:57:45 -0300

    refactor: change image in profile

--- src/screens/user-details/index.tsx
+++ src/screens/user-details/index.tsx
@@ -1,11 +1,37 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useUserStore } from "../../store/user";
 import { useTokenStore } from "../../store/token";
-import { Box, ScrollView, Text, VStack, Heading, Button } from "native-base";
+import { Box, ScrollView, Text, VStack, Button } from "native-base";
 
 const UserDetails = () => {
   const { user, setUser } = useUserStore();
   const { setToken } = useTokenStore();
+
+  function handleColorsBackground(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const numberColor = Math.floor(Math.random() * (max - min) + min);
+    return `darkBlue.${numberColor}00`
+  }
+
+  function handleArrayName(names: string[]) {
+    if (names.length > 2) {
+      names.pop();
+      handleArrayName(names);
+    }
+    return names;
+  }
+
+  function handleInitialsNames(name: string) {
+    name = name.replace(/\s(de|da|dos|das)\s/g, " "); // Remove os de,da, dos,das.
+    const nameArray = name.split(" ");
+    const newNames = handleArrayName(nameArray);
+    if (newNames.length < 2) return newNames[0][0].toUpperCase();
+
+    return newNames[0][0].toUpperCase() + newNames[1][0].toUpperCase();
+  }
+
+  const initalsName = handleInitialsNames(user.name);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
@@ -17,11 +43,17 @@ const UserDetails = () => {
                 alignItems="center"
                 justifyContent="center"
                 height="150"
-                backgroundColor="darkBlue.800"
+                bg={{
+                  linearGradient: {
+                    colors: [handleColorsBackground(1, 10),handleColorsBackground(1, 10)],
+                    start: [0, 0],
+                    end: [1, 0],
+                  },
+                }}
                 width="39%"
                 borderRadius={150}
               >
-                <Text fontSize="7xl">{user.name[0]}</Text>
+                <Text fontSize="6xl">{initalsName}</Text>
               </Box>
 
               <Text fontSize="2xl">{user.name}</Text>
